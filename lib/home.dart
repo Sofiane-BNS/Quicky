582e617f5bc16be4e52b667e07da9e5c3c742ffd
agadachach <amine.gadacha-charrad@insa-lyon.fr>
2020-04-17 11:32:55 +0200

    Ajout structure panier

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'cart.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _HomeState();
   }
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Acceuil'),
-      )
-    );
+        appBar: AppBar(
+      title: Text('Acceuil'),
+      actions: <Widget>[
+        new IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => new Cart()));
+          },
+        )
+      ],
+    ));
   }
 }
