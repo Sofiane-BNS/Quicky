3b7df5238ac5d66c811f9a2d2bfc0739f8f8aa40
jbrouboni <45970166+jbrouboni@users.noreply.github.com>
2020-04-27 10:44:48 +0200

    Deconnexion(ok) + Menu

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quicky/login_page.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -12,8 +13,62 @@ class _HomeState extends State<Home>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Acceuil'),
-      )
+        title: Text('Accueil'),
+        backgroundColor: Colors.orange,
+        actions: <Widget>[
+          new IconButton(icon:Icon(Icons.shopping_cart, color:Colors.white) , onPressed: (){})
+        ],
+      ),
+        drawer: new Drawer(
+          child: ListView(
+            children: <Widget>[
+
+              new UserAccountsDrawerHeader(
+                  accountName: Text('Joël Boni'),
+                  accountEmail: Text('joel@gmail.com'),
+                  currentAccountPicture: GestureDetector(
+                    child: new CircleAvatar(
+                      backgroundColor: Colors.orange,
+                      child:Icon(Icons.person, color:Colors.white),
+                    ),
+                  ),
+                decoration: new BoxDecoration(
+                  color:Colors.orange
+                ),
+
+              ),
+             ListTile(
+               title:Text('Home'),
+             leading:Icon(Icons.home),
+             ),
+              InkWell(
+                onTap:(){} ,
+                child: ListTile(
+                  title:Text('Mon compte'),
+                  leading:Icon(Icons.person),
+                ),
+              ),
+              InkWell(
+                onTap:(){} ,
+                child: ListTile(
+                  title:Text('Parametres'),
+                  leading:Icon(Icons.settings),
+                ),
+              ),
+              InkWell(
+                onTap:(){
+                  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                    LoginPage()), (Route<dynamic> route) => false);} ,
+
+                child: ListTile(
+                  title:Text('Deconnexion'),
+                  leading:Icon(Icons.exit_to_app),
+
+                ),
+              )
+    ],
+    ),
+        ),
     );
   }
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quicky/sign_up_page.dart';
 
